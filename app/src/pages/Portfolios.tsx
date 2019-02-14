c21bc7fddb576744075d67b720d5fb2b402927a3
Lisandro Corbalan <lmcorbalan@gmail.com>
2019-02-14 19:35:36 -0300

    Add section title

--- app/src/pages/Portfolios.tsx
+++ app/src/pages/Portfolios.tsx
@@ -10,6 +10,7 @@ const Portofios = () => {
     <Layout>
       <NavBar />
       <Content>
+        <Title>Your Portfolios</Title>
         <Section>
           <AddPortfolio />
           <Portfolio />
@@ -20,9 +21,17 @@ const Portofios = () => {
 };
 
 const Section = styled.section`
-  padding: var(--spacing-normal);
+  padding: var(--spacing-normal) 10%;
   display: flex;
   justify-content: space-around;
+`;
+
+const Title = styled.header`
+  padding-top: 1.5em;
+  padding-bottom: 0.2em;
+  border-bottom: 1px solid var(--color-border);
+  margin: 0 10%;
+  line-height: 1.33;
 `;
 
 export default Portofios;
